2beba48837b5aea4f4a69431dada70431f803d9b
msamgan <mohdsamgankhan@gmail.com>
2022-10-22 23:38:21 -0400

    updated env

--- next.config.js
+++ next.config.js
@@ -6,9 +6,10 @@ const nextConfig = {
         domains: ["secure.gravatar.com", "cdn.ko-fi.com", "codebysamgan.com"]
     },
     env: {
+        APP_ENV: process.env.NEXT_PUBLIC_APP_ENV,
         APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
         API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-        BACKEND_BASE_URL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
+        BACKEND_BASE_URL: process.env.NEXT_PUBLIC_BACKEND_BASE_URL
     }
 }
 
