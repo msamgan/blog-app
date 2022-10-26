5ce9dc14d378be60e98ab84aeb6f89659b44b25a
msamgan <mohdsamgankhan@gmail.com>
2022-10-26 01:30:24 -0400

    added giscus comments

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document"
+import Script from "next/script"
 
 export default function Document() {
     return (
@@ -7,7 +8,6 @@ export default function Document() {
             <body>
                 <Main />
                 <NextScript />
-
                 {/* Global Site Tag (gtag.js) - Google Analytics */}
                 <script
                     dangerouslySetInnerHTML={{
